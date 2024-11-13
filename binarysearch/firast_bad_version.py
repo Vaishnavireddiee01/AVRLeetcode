ec64d481b6e66ccc02d14a03aa9cf995121d3ef9
Vaishnavireddiee01 <135511458+Vaishnavireddiee01@users.noreply.github.com>
2024-11-13 21:57:07 +0530

    Add files via upload

---
+++ binarysearch/firast_bad_version.py
@@ -0,0 +1,43 @@
+"""Q:You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+ 
+
+Example 1:
+
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version."""
+
+
+
+# Simulated API for testing
+def isBadVersion(version: int) -> bool:
+    # Set the first bad version for testing purposes
+    return version >= bad_version
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l, r = 1, n
+        while l < r:
+            m = (l + r) // 2
+            if isBadVersion(m):
+                r = m
+            else:
+                l = m + 1
+        return l
+
+# Driver code
+if __name__ == "__main__":
+    n = 5  # Total number of versions
+    bad_version = 4  # The first bad version for this test case
+    solution = Solution()
+    result = solution.firstBadVersion(n)
+    print(f"The first bad version is: {result}")
